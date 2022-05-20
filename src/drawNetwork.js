cba29aeb1c1e31862f5176bc3abd5c86d7801228
Marco Exp <marcoexp@ucm.es>
2022-05-20 17:55:56 +0200

    Coloreado de nodos
    El color de los nodos depende del valor de explicit_community.
    El nombre de la clave dentro del codigo es un atributo facil de cambiar.
    El color de cada grupo dentro del código es un atributo facil de cambiar.

--- src/drawNetwork.js
+++ src/drawNetwork.js
@@ -2,8 +2,17 @@ import { DataSet } from "vis-data/peer";
 import { Network } from "vis-network/peer";
 
 export default class DrawNetwork {
+
+
     constructor(jsonInput, container) {
         this.container = container;
+
+        //In case explicit_community key changes, this is what is compared while parsing edges json
+        this.groupColor_key = "explicit_community";
+        
+        this.groupColor_0 = "#4ADEDE";
+        this.groupColor_1 = "#37983B";
+        this.groupColor_2 = "#FFFFFF";   
 
         this.parseJson(jsonInput);
         this.chooseOptions(jsonInput);
@@ -14,42 +23,82 @@ export default class DrawNetwork {
 
 
     parseJson(json) {
-
         //Get nodes from json
-
-        const nodes = new DataSet([
-            { id: 1, label: "Node 1" },
-            { id: 2, label: "Node 2" },
-            { id: 3, label: "Node 3" },
-            { id: 4, label: "Node 4" },
-            { id: 5, label: "Node 5" }
-        ]);
-
-        //Get edges from json
-
-        const edges = new DataSet([
-            { from: 1, to: 3 },
-            { from: 1, to: 2 },
-            { from: 2, to: 4 },
-            { from: 2, to: 5 },
-            { from: 3, to: 3 }
-        ]);
+        const nodes = this.parseNodes(json);
+        const edges = this.parseEdges(json);
 
         this.data = {
             nodes: nodes,
-        	edges: edges
+            edges: edges
         };
     }
 
-    chooseOptions(){
-        this.options = {};
+    parseNodes(json) {
+        for (const node of json.users) {
+            node["oldgroup"] = node["group"];   //Just in case we need the group in the future
+
+            node["group"] = "group_" + node[this.groupColor_key]
+            delete node[this.groupColor_key];
+
+            console.log(node);
+        }
+        const nodes = new DataSet(json.users);
+        return nodes;
     }
+
+    parseEdges(json) {
+        //Get edges from json
+        for (const edge of json.similarity) {
+            edge["from"] = edge["u1"];
+            delete edge["u1"];
+
+            edge["to"] = edge["u2"];
+            delete edge["u2"];
+        }
+        const edges = new DataSet(json.similarity);
+        return edges;
+    }
+
+    chooseOptions() {
+        this.options = {
+            edges: {
+                scaling: {
+                    min: 3,
+                    max: 15
+                }
+            },
+            nodes: {
+                shape: 'circle',
+                widthConstraint: 30,
+                font: {
+                    size: 20
+                },
+            },
+            groups: {
+                group_0: {
+                    color: {
+                        background: this.groupColor_0
+                    }
+                },
+                group_1: {
+                    color: {
+                        background: this.groupColor_1
+                    }
+                },
+                group_2: {
+                    color: {
+                        background: this.groupColor_2
+                    }
+                }
+            }
+        };
+    }
+
     drawNetwork() {
         this.network = new Network(this.container, this.data, this.options);
     }
 
-
-    clearNetwork(){
+    clearNetwork() {
         this.network.destroy();
     }
 }
