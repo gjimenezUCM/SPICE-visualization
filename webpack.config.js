2c0518a52a38dd20791d386848f6ca99ae3fc086
Marco Exp <marcoexp@ucm.es>
2022-06-06 10:48:04 +0200

    Webpack options fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
-    initExplicit0_Colors() {
-        //Explicit Community 0
-        this.explicit0_Key = this.explicit_filter[0].key;
+const path = require('path');
 
-        const n = this.explicit_filter[0].values.length;
-        const colors = this.explCommOptions.getColorsForN(n);
-
-        this.nodeColors = new Map();
-        for (let i = 0; i < n; i++) {
-            this.nodeColors.set(this.explicit_filter[0].values[i], colors[i]);
-        }
-    }
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '/')
+        },
+        port: 1234
+    },
+};
